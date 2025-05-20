36a8ddfe2023128eaba8815c55638d8f1fddcfba
Erin Rivas <me@erinrivas.com>
2025-05-20 15:02:44 -0400

    client instantiation rework

--- src/jackalClient.ts
+++ src/jackalClient.ts
@@ -1,46 +1,61 @@
-import {ClientHandler, IClientHandler, IStorageHandler, StorageHandler,} from "@jackallabs/jackal.js";
-import dotenv from "dotenv";
-import {testnet} from "./config";
+import {
+  ClientHandler,
+  IClientHandler,
+  IStorageHandler,
+  StorageHandler,
+  TWalletExtensionNames,
+} from '@jackallabs/jackal.js'
+import dotenv from 'dotenv'
+import { mainnet, testnet } from './config'
 
-export const BASE_FOLDER = "test";
+dotenv.config()
 
-dotenv.config();
+export async function initJackal() {
+  let clientHandler: IClientHandler
+  let storageHandler: IStorageHandler
 
-export async function initJackalClients(keyEnvVar: string = "JKL_SECRET_KEY1") {
-    let clientHandler: IClientHandler;
-    let storageHandler: IStorageHandler;
+  const BASE_FOLDER = process.env.JACKAL_FOLDER || ''
 
-    try {
-        clientHandler = await ClientHandler.connect({
-            ...testnet,
-            selectedWallet: "mnemonic",
-            mnemonic: process.env[keyEnvVar],
-        });
-        storageHandler = await StorageHandler.init(clientHandler, {
-            setFullSigner: true,
-        });
-
-        await storageHandler.initStorage();
-        // try {
-        //   await openFolder(storageHandler, `Home/${BASE_FOLDER}`);
-        // } catch (err) {
-        //   console.log(`Creating storage root: ${BASE_FOLDER}`);
-        //   // Create S3 root folder
-        //   await storageHandler.createFolders({ names: BASE_FOLDER });
-        //   await openFolder(storageHandler, "Home");
-        // }
-
-        console.log("Jackal.js client initialized successfully");
-
-        await storageHandler.loadProviderPool();
-
-        await storageHandler.upgradeSigner();
-
-        console.log(`[INFO] - Storage Handler initialized with ${keyEnvVar}`);
-
-        return {clientHandler, storageHandler};
-    } catch (error) {
-        console.error("Failed to initialize Jackal.js client:", error);
-        process.exit(1);
+  let pkg
+  if (process.env.CHAIN_MODE === 'mainnet') {
+    pkg = {
+      ...mainnet,
+      selectedWallet: 'mnemonic' as TWalletExtensionNames,
+      mnemonic: process.env.MAINNET_MNEMONIC || '',
     }
+  } else {
+    pkg = {
+      ...testnet,
+      selectedWallet: 'mnemonic' as TWalletExtensionNames,
+      mnemonic: process.env.TESTNET_MNEMONIC || '',
+    }
+  }
+
+
+  try {
+    clientHandler = await ClientHandler.connect(pkg)
+    storageHandler = await StorageHandler.init(clientHandler, {
+      setFullSigner: true,
+    })
+
+    await storageHandler.initStorage()
+    try {
+      await storageHandler.loadDirectory({ path: `Home/${BASE_FOLDER}` })
+    } catch {
+      console.log(`Creating storage root: ${BASE_FOLDER}`);
+      // Create S3 root folder
+      await storageHandler.createFolders({ names: BASE_FOLDER });
+      await storageHandler.loadDirectory({ path: `Home/${BASE_FOLDER}` })
+    }
+
+    console.log('Jackal.js client initialized successfully')
+
+    await storageHandler.loadProviderPool()
+
+    return { storageHandler, workingHome: `Home/${BASE_FOLDER}` }
+  } catch
+    (err) {
+    console.error('Failed to initialize Jackal.js client:', err)
+    process.exit(1)
+  }
 }
